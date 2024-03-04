03175f661e2c99faaeccb1660606d3bcca141ea6
Masrur <104124372+masrur-qr@users.noreply.github.com>
2024-03-04 18:29:04 +0500

    Add mu name into print main.go

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello masrur Backend")
-	fmt.Println("Hello Backend Second")
+	fmt.Println("Hello Backend Second jahongir")
 	fmt.Println("Hello Backend 3")
 	fmt.Println(" ----------------------------- -")
 	fmt.Println(" ++++++++++++++++++++++++++++-")
