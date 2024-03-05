fd6f67cde548c7b55cb521a4df09d8112f90520b
masrur <you@example.com>
2024-03-05 23:32:36 +0500

    conflict test

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello masrur Backend")
-	fmt.Println("Hello Backend Second jahongir")
-	fmt.Println("Hello Backend 3")
-	fmt.Println(" ----------------------------- -")
-	fmt.Println(" ++++++++++++++++++++++++++++-")
+	fmt.Println("Salomet mund mu nom ---- ")
 }
