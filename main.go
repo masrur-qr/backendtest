02383c60feff1e04cd2c7ba4c48692a5507cebf5
masrur <you@example.com>
2024-03-06 15:34:15 +0500

    old conflict stuff

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Salomet mund mu nom khayom ")
-	fmt.Println("Hello to masrur branch")
-	var myName string = "Masrur"
-
-	fmt.Printf("myName: %v\n", myName)
+	fmt.Println("Salomet mund mu nom ------ ")
 }
